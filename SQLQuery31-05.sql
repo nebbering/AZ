de88feefec12ed363a4bad0d92b7b683f8308ca2
Luan H. Nebbering <78267195+nebbering@users.noreply.github.com>
2022-05-31 10:59:49 -0300

    update

---
+++ SQLQuery31-05.sql
@@ -0,0 +1,25 @@
+--create table lista (
+--	idCli int IDENTITY(1,1) NOT NULL,
+--	nome varchar (150) NOT NULL,
+--	apelido varchar (20),
+--	tel varchar (14) NOT NULL,
+--	email varchar (150),
+--	endereco varchar (300),
+--);
+
+--select * from lista
+--update lista set nome = 'OKABE'
+
+--insert into lista (
+--nome,
+--apelido,
+--tel,
+--email,
+--endereco)
+
+--Values (
+--'Okabe Rintaru',
+--'Hououim Kyouma',
+--'81 996275232',
+--'kyouma@gmail.com',
+--'Av. Akihabara, 97 - Akihabara, Tokio, Japan');
